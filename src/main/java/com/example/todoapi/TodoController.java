f81d6fb89d02dbeafd2a8c5812c3a6695abbb668
aryan <aryananand12517@gmail.com>
2025-01-23 14:02:57 +0530

    Added Delete Request and Not Found Message

--- src/main/java/com/example/todoapi/TodoController.java
+++ src/main/java/com/example/todoapi/TodoController.java
@@ -2,15 +2,18 @@ package com.example.todoapi;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     private static List<Todo> todolist;
+    private static final String TODO_NOT_FOUND = "Todo not found";
 
     public TodoController(){
         todolist= new ArrayList<>();
@@ -18,16 +21,48 @@ public class TodoController {
         todolist.add(new Todo(2,true,"Todo 2",2));
     }
 
-    @GetMapping("/todos")
+    @GetMapping
     public  List<Todo> getTodos(){
         return  todolist;
     }
 
 
-        @PostMapping("/todos")
+        @PostMapping
         @ResponseStatus(HttpStatus.CREATED)
         public Todo createTodo(@RequestBody Todo newTodo){
         todolist.add(newTodo);
         return newTodo;
         }
+
+        @GetMapping("/{todoId}")
+        public ResponseEntity<?> getTodoById(@PathVariable Long todoId){
+         for(Todo todo : todolist){
+             if(todo.getId()==todoId){
+                 return ResponseEntity.ok(todo);
+             }
+         }
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+        }
+
+        @DeleteMapping("/{todoId}")
+        public ResponseEntity<?> deleteById(@PathVariable Long todoId)
+        {
+            Todo todoToRemove=null;
+            for(Todo todo: todolist){
+                if(todo.getId()==todoId)
+                {
+                    todoToRemove=todo;
+                    break;
+                }
+            }
+            if(todoToRemove!=null) {
+                todolist.remove(todoToRemove);
+                String deleteSuccessMessage = "Todo deleted Successfully";
+                return ResponseEntity.status(HttpStatus.OK).body(deleteSuccessMessage);
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+            }
+        }
+
+
 }
